e2028230693c93d8960cd691b2bf24f81abe84e1
gitmaster <master@example.com>
2024-10-05 08:46:29 +0530

    terraform filed added

---
+++ subnet.tf
@@ -0,0 +1,6 @@
+resource "aws_subnet" "sub1" {
+
+   vpc_id = aws_vpc.myvpc.id
+   cidr_block = "192.168.0.0/26"
+   availability_zone = "us-east-1a"
+}
---
+++ vpc.tf
@@ -0,0 +1,5 @@
+resource "aws_vpc" "myvpc" {
+
+  cidr_block = "192.168.0.0/24"
+
+}
